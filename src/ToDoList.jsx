f6b036f958df39c448a896b05f67900ef1f3e00d
larriag13 <119143023+larriag13@users.noreply.github.com>
2026-06-10 13:50:32 +0000

    avance 1 todo

---
+++ src/ToDoList.jsx
@@ -0,0 +1,54 @@
+import { useState, useRef, useEffect } from "react";
+import {v4 as uuid} from 'uuid';//npm i uuid
+
+const KEY="tareas-miercoles";
+
+function ToDoList(){
+    
+    const [todos,setTodos]=useState(
+        JSON.parse(localStorage.getItem(KEY))?JSON.parse(localStorage.getItem(KEY)):[]
+    );
+
+    const taskRef=useRef();
+
+    useEffect(()=>{
+        localStorage.setItem(KEY,JSON.stringify(todos));
+        },[todos]);//cuando cambie el todos lo guarda en localStorage
+    
+    const agregarTarea=()=>{
+        const task=taskRef.current.value;
+        const id=uuid();
+        console.log(id);
+        console.log(task);
+        if(task==='')return;
+        setTodos((prevTodos)=>{
+            const newTask = {
+                id: id,
+                task: task,
+                complete:false
+            }
+            return [...prevTodos, newTask]
+        });
+        taskRef.current.value="";
+    }
+
+    const eliminarTareasCompletadas=()=>{
+        const newTodos=todos.filter((todo)=>!todo.complete);
+        setTodos(newTodos);
+    }
+
+    return(
+        <>
+            <h1>Listado de Tareas</h1>
+            <div className="input-group mb-3 mt-4">
+                <input ref={taskRef} placeholder="Ingrese una tarea" className="form-control" type="text" name="" id="">
+                </input>{/*  soy un comentario  */}
+                <button onClick={agregarTarea} className="btn btn-success ms-2"><i className="bi bi-plus-circle-fill">
+                    </i></button>
+                <button onClick={eliminarTareasCompletadas} className="btn btn-danger ms-2"><i className="bi bi-trash">
+                    </i></button>
+            </div>
+        </> 
+    );
+}
+export default ToDoList;
